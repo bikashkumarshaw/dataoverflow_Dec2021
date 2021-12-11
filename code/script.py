fba4cde34eba61c0231c2fa0010c6ff538949744
Bikash shaw <32449999+bikashkumarshaw@users.noreply.github.com>
2021-12-11 17:36:17 +0530

    my changes

--- code/script.py
+++ code/script.py
@@ -1,15 +1,77 @@
+import os
+import pandas as pd
+from datetime import datetime
 
+VALID_VACCINES = ['a', 'b', 'c']
+START_DATE = datetime.strptime('01-02-2020', "%d-%m-%Y")
+END_DATE = datetime.strptime('30-11-2021', "%d-%m-%Y")
 
 def covid_vaccine(vaccination_status_files, user_meta_file, output_file):
-    """This function takes  vaccination_status_files and user_meta_file paths
-    using this all the covid vaccination numbers needs to be stored in the given output file as TSV
-    Args:
-        vaccination_status_files: A List containing file path to the TSV vaccination_status_file.
-        user_meta_file: A file path to TSV file containing User information.
-        output_file: File path where output TSV results are should be stored, 
-    Returns:
-      None (doesnt return anything)
-    """
-    # your code goes here.
-    #  feel free to modify this entire script, except this function signature.
-    pass
+    df = pd.DataFrame()
+    for fl in vaccination_status_files:
+        if not os.path.exists(fl):
+            continue
+        vaccine_df = pd.read_csv(fl, sep='\t', header=0)
+        for idx, row in vaccine_df.iterrows():
+            user, vaccine, date = row
+            if not is_vaccine_data_valid(user, vaccine, date):
+                vaccine_df.drop(idx, inplace=True)
+
+        df = pd.concat([df, vaccine_df], ignore_index=True)
+
+    user_df = pd.read_csv(user_meta_file, sep='\t', header=0)
+    for idx, row in user_df.iterrows():
+        user, gender, city, state = row
+
+        if not is_user_data_valid(user, gender, city, state):
+            user_df.drop(idx, inplace=True)
+
+    if df.empty:
+        df.to_csv(output_file, index=False, sep='\t')
+        return
+
+    df = df.set_index('user')
+    user_df = user_df.set_index('user')
+    df.pop('date')
+    my_df = df.join(user_df, how='inner', on='user')
+    my_df = my_df.reset_index().drop_duplicates()
+    my_df.pop('user')
+    my_df = my_df.groupby(['city', 'state', 'vaccine', 'gender'], as_index=True).size()
+    my_df = my_df.reset_index()
+    my_df.rename(columns={"size": "unique_vaccinated_people"}, inplace=True)
+    my_df.to_csv(output_file, index=False, sep='\t')
+
+def is_user_data_valid(user, gender, city, state):
+    if not (isinstance(gender, str) and isinstance(city, str) and isinstance(state, str)):
+        return False
+
+    # Try typecasting
+    try:
+        int(user)
+    except:
+        return False
+
+    return True
+
+def is_vaccine_data_valid(user, vaccine, date):
+    if not isinstance(vaccine, str):
+        return False
+
+    # Try typecasting
+    try:
+        int(user)
+    except:
+        return False
+
+    try:
+        date = datetime.strptime(date, "%d-%m-%Y")
+    except:
+        return False
+
+    if vaccine.lower() not in VALID_VACCINES:
+        return False
+
+    if date < START_DATE or date > END_DATE:
+        return False
+
+    return True
